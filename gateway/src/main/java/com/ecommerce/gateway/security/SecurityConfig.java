f80dad9173be46c3f31a32df4f8785cc14da14f9
Denis Githuku <denisgithuku07@gmail.com>
2026-01-02 15:55:18 +0300

    Extract role from jwt.

--- gateway/src/main/java/com/ecommerce/gateway/security/SecurityConfig.java
+++ gateway/src/main/java/com/ecommerce/gateway/security/SecurityConfig.java
@@ -2,10 +2,21 @@ package com.ecommerce.gateway.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -17,8 +28,21 @@ public class SecurityConfig {
         httpSecurity.authorizeExchange(exchange -> exchange.anyExchange().authenticated());
         return httpSecurity // Every client has to provide a token before they access anything
                 .oauth2ResourceServer(oAuth2ResourceServerSpec -> {
-                    oAuth2ResourceServerSpec.jwt(Customizer.withDefaults());
+                    oAuth2ResourceServerSpec.jwt(jwtSpec -> jwtSpec.jwtAuthenticationConverter(grantedAuthoritiesExtractor()));
                 })
                 .build();
+    }
+
+    private Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
+        ReactiveJwtAuthenticationConverter jwtAuthenticationConverter = new ReactiveJwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            List<String> roles = jwt.getClaimAsMap("resource_access")
+                    .entrySet().stream()
+                    .filter(entry -> entry.getKey().equals("oauth2-pkce"))
+                    .flatMap(entry -> ((Map<String, List<String>>) entry.getValue()).get("roles").stream()).toList();
+            System.out.println("Extracted roles: " + roles);
+            return Flux.fromIterable(roles).map(role -> new SimpleGrantedAuthority("ROLE_"+role));
+        });
+        return jwtAuthenticationConverter;
     }
 }
